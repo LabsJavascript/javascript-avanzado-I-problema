a9d2a4145d1a1043f1bd35ddc99cc95698e7005a
Rob <robertdacorte@gmail.com>
2023-09-07 02:05:19 +0200

    solution, refactor and cleaning

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 import { urls } from "./constant.js";
+import { goToPostDetail } from "./detailPost.js";
 import { formatDataAgo } from "./utils.js";
 
 (async function () {
@@ -42,7 +43,7 @@ import { formatDataAgo } from "./utils.js";
 		postContent.innerHTML = post.content;
 		postContent.className = "post-content"
 
-		postLink.addEventListener('click', goToPostDetail)
+		postLink.addEventListener('click', (e) => goToPostDetail(e, loader))
 
 		postHeader.appendChild(postHeaderTitle)
 		postFooter.appendChild(postContent)
@@ -53,57 +54,4 @@ import { formatDataAgo } from "./utils.js";
 		return postElemment
 	}
 
-	function goBack() {
-		const postDetail = document.querySelector('.detail-post');
-		const posts = document.querySelector('.posts');
-		postDetail.innerHTML = '';
-		postDetail.classList.add('hidden');
-		posts.classList.remove('hidden');
-	}
-
-	function goToPostDetail(e) {
-		e.preventDefault();
-		const id = e.currentTarget.dataset.id;
-		loader.classList.remove('hidden');
-		fetch(urls[id])
-			.then(res => res.json())
-			.then(post => generatePostDetail(post))
-		// const post = posts.find(post => post.id === id);
-	}
-	function generatePostDetail(post) {
-		const postDetail = document.querySelector('.detail-post');
-		const postsSection = document.querySelector('.posts');
-		const backButton = document.createElement('button');
-		const headerDetail = document.createElement('header');
-		backButton.classList.add('back-button');
-		headerDetail.classList.add('detail-post-header');
-		const backButtonIcon = document.createElement('i');
-		backButtonIcon.classList.add('fas', 'fa-arrow-left', 'fa-solid', 'green');
-		backButton.appendChild(backButtonIcon);
-		backButton.addEventListener('click', goBack);
-		postDetail.appendChild(backButton);
-
-		postDetail.classList.remove('hidden');
-		postsSection.classList.add('hidden');
-
-		const postDetailText = document.createElement('p');
-		postDetailText.classList.add('detail-post-content');
-		const postDetailTitle = document.createElement('h2');
-		const postAuthor = document.createElement('p');
-
-		postDetailTitle.textContent = post.title;
-
-
-		postDetailText.innerHTML = post.content.replace(/\n/g, '<br />');
-		postAuthor.classList.add('detail-post-author');
-		postAuthor.textContent = post.author;
-
-		headerDetail.appendChild(backButton);
-		headerDetail.appendChild(postDetailTitle);
-		postDetail.appendChild(headerDetail);
-		postDetail.appendChild(postDetailText);
-		loader.classList.add('hidden');
-		postDetail.appendChild(postAuthor);
-
-	}
 })()
